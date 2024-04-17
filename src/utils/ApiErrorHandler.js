6b592369a7b8dc1cc5541c6b83a57d0863f819e8
avizith-debnath :) <at.avijit.deb@gmail.com>
2024-04-17 23:36:11 +0600

    async method, api response and error handler is added

---
+++ src/utils/ApiErrorHandler.js
@@ -0,0 +1,21 @@
+class ApiErrorHandler extends Error {
+    constructor(
+        statusCode,
+        message= "Something went wrong",
+        errors = [],
+        stack = ""
+    ){
+        super(message);
+        this.success = false;
+        this.data = null;
+        this.statusCode = statusCode;
+        this.errors = errors;
+        if(stack){
+            this.stack = stack;
+        } else {
+            Error.captureStackTrace(this, this.constructor);
+        }
+    }
+}
+
+export { ApiErrorHandler };
